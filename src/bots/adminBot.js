deeb1f5bc6d723f5c6bf7774038510ceb2e02fbd
ot2dz <must2dz@gmail.com>
2024-07-25 18:19:33 +0100

    admin bot

--- src/bots/adminBot.js
+++ src/bots/adminBot.js
@@ -168,10 +168,10 @@ bot.onText(/\/getAllRides/, async (msg) => {
     if (rides.length > 0) {
       let response = 'قائمة الرحلات:\n';
       response += '```\n';
-      response += 'ID            | الزبون        | السائق       | الحالة      \n';
-      response += '--------------|--------------|--------------|--------------\n';
+      response += 'الزبون        | السائق       | العنوان      \n';
+      response += '--------------|--------------|--------------\n';
       rides.forEach(ride => {
-        response += `${(ride._id.toString() || '-').padEnd(14)} | ${(ride.userPhone || '-').padEnd(12)} | ${(ride.driverName || '-').padEnd(12)} | ${(ride.userAddress || '-').padEnd(12)}\n`;
+        response += `${(ride.userPhone || '-').padEnd(12)} | ${(ride.driverName || '-').padEnd(12)} | ${(ride.userAddress || '-').padEnd(12)}\n`;
       });
       response += '```';
       bot.sendMessage(chatId, response, { parse_mode: 'Markdown' });
