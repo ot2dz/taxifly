f57e2080522df53d70b6213187ce51c84948bb85
ot2dz <must2dz@gmail.com>
2024-07-28 15:59:34 +0100

    admin appreved

--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ module.exports = {
   DRIVER_BOT_TOKEN: process.env.DRIVER_BOT_TOKEN,
   MONGODB_URI: process.env.MONGODB_URI,
   ADMIN_BOT_TOKEN: process.env.ADMIN_BOT_TOKEN,
+  ADMIN_CHAT_ID: process.env.ADMIN_CHAT_ID,
   PORT: process.env.PORT || 3000
 };
